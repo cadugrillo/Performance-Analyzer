46b4f3a8ed7215a7802fe68a302d2c0fc60bce5b
Cadu Grillo <cadugrillo@gmail.com>
2022-10-10 17:05:03 -0400

    improved algorithm

--- modules/analyze-captured-data/analyze-captured-data.go
+++ modules/analyze-captured-data/analyze-captured-data.go
@@ -106,56 +106,58 @@ func AnalyzeData(TsInterval int64) {
 			break
 		}
 
-		if strings.Contains(telegrams[i].Topic, "timeseries_json_generic") {
+		if !strings.Contains(telegrams[i].Topic, "timeseries_json_generic") {
+			continue
+		}
 
-			fmt.Println("Starting analysis with telegram Seq = ", telegrams[i].Payload.Payload.Seq)
+		fmt.Println("Starting analysis with telegram Seq = ", telegrams[i].Payload.Payload.Seq)
 
-			for j := 0; j < len(telegrams[i].Payload.Payload.Vals); j++ {
+		for j := 0; j < len(telegrams[i].Payload.Payload.Vals); j++ {
 
-				for p := 0; p < len(checkedIds); p++ {
-					if telegrams[i].Payload.Payload.Vals[j].Id == checkedIds[p] {
-						idAlreadyChecked = true
-						break
-					}
+			for p := 0; p < len(checkedIds); p++ {
+				if telegrams[i].Payload.Payload.Vals[j].Id == checkedIds[p] {
+					idAlreadyChecked = true
+					break
 				}
+			}
 
-				if !idAlreadyChecked {
-
-					id = telegrams[i].Payload.Payload.Vals[j].Id
-					issue.SignalId = fmt.Sprintf("%s - from Telegram Seq number = %d", telegrams[i].Payload.Payload.Vals[j].Id, telegrams[i].Payload.Payload.Seq)
-					tsRFC3339, _ := time.Parse(time.RFC3339, telegrams[i].Payload.Payload.Vals[j].Ts)
-					ts = tsRFC3339.UnixMilli()
-					tsOffset = 200
-
-					for k := i; k < len(telegrams); k++ {
-
-						for l := 0; l < len(telegrams[k].Payload.Payload.Vals); l++ {
-
-							if telegrams[k].Payload.Payload.Vals[l].Id == id {
-
-								cmptsRFC3339, _ := time.Parse(time.RFC3339, telegrams[k].Payload.Payload.Vals[l].Ts)
-								cmpts := cmptsRFC3339.UnixMilli()
-
-								if (cmpts - ts) > TsInterval+tsOffset {
-
-									errorFlag = true
-									Message := fmt.Sprintf("Missing records near seq: %d", telegrams[k].Payload.Payload.Seq)
-									issue.Messages = append(issue.Messages, Message)
-									fmt.Println("Missing records from id: ", id, "near seq: ", telegrams[k].Payload.Payload.Seq)
-								}
-								ts = cmpts
-								checkedIds = append(checkedIds, id)
-							}
-						}
-					}
-					if errorFlag {
-
-						analyzedData.Issues = append(analyzedData.Issues, issue)
-						issue = Issue{}
-						errorFlag = false
-					}
-				}
+			if idAlreadyChecked {
 				idAlreadyChecked = false
+				continue
+			}
+
+			id = telegrams[i].Payload.Payload.Vals[j].Id
+			issue.SignalId = fmt.Sprintf("%s - from Telegram Seq number = %d", telegrams[i].Payload.Payload.Vals[j].Id, telegrams[i].Payload.Payload.Seq)
+			tsRFC3339, _ := time.Parse(time.RFC3339, telegrams[i].Payload.Payload.Vals[j].Ts)
+			ts = tsRFC3339.UnixMilli()
+			tsOffset = 200
+
+			for k := i; k < len(telegrams); k++ {
+
+				for l := 0; l < len(telegrams[k].Payload.Payload.Vals); l++ {
+
+					if telegrams[k].Payload.Payload.Vals[l].Id == id {
+
+						cmptsRFC3339, _ := time.Parse(time.RFC3339, telegrams[k].Payload.Payload.Vals[l].Ts)
+						cmpts := cmptsRFC3339.UnixMilli()
+
+						if (cmpts - ts) > TsInterval+tsOffset {
+
+							errorFlag = true
+							Message := fmt.Sprintf("Missing records near seq: %d", telegrams[k].Payload.Payload.Seq)
+							issue.Messages = append(issue.Messages, Message)
+							fmt.Println("Missing records from id: ", id, "near seq: ", telegrams[k].Payload.Payload.Seq)
+						}
+						ts = cmpts
+					}
+				}
+			}
+			checkedIds = append(checkedIds, id)
+			if errorFlag {
+
+				analyzedData.Issues = append(analyzedData.Issues, issue)
+				issue = Issue{}
+				errorFlag = false
 			}
 		}
 	}
@@ -166,4 +168,5 @@ func AnalyzeData(TsInterval int64) {
 		analysisStatus = "Finished"
 	}
 	abortAnalysis = false
+	fmt.Println("Analyzed Record IDs:", checkedIds)
 }
